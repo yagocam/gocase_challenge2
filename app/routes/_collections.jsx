fe4e78db8f9e945735e41fc836e9ca8a4d51e77e
yago <yagocam5@gmail.com>
2023-12-13 22:50:01 -0300

    criação de produtos

--- app/routes/_collections.jsx
+++ app/routes/_collections.jsx
@@ -5,26 +5,30 @@ import CollectionList from '../components/CollectionList';
 const Collections = () => {
   const [collectionData, setCollectionData] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/collections',);
+      const data = await response.json();
+
+      console.log('Received data:', data.data);
+
+      setCollectionData(data.data?.collections?.nodes || []);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/collections');
-        const data = await response.json();
-
-        console.log('Received data:', data.data);  // Adicione esta linha para inspecionar os dados
-
-       
-          setCollectionData(data.data?.collections?.nodes || []);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const handleRefreshClick = () => {
+    fetchData();
+  };
+
   return (
     <div>
+      <button onClick={handleRefreshClick}>Refresh Collections</button>
       <h1>Collections Page</h1>
       {collectionData ? (
         <CollectionList collections={collectionData} />
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,28 +1,64 @@
 // src/routes/index.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ProductList from '../components/ProductList';
 
-// Interface para a forma dos dados dos produtos
 const Index = () => {
+  const [limit, setLimit] = useState(10);
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/images'); // Atualize a URL conforme necessário
-        const data = await response.json();
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/products?limit=${limit}`);
+      const data = await response.json();
+      setProducts(data.data?.products?.edges || []);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-        // Diretamente definir o estado products com a resposta
-        setProducts(data.data?.products?.edges || []);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const sendData = async () => {
+    const url = 'http://localhost:3000/createProduct';
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: 'Produto Teste',
+          status: 'active',
+          descriptionHtml: 'Descrição do produto...',
+        }),
+      });
+  
+      const data = await response.json();
+      console.log('Response:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
+  const handleButtonClick = () => {
     fetchData();
-  }, []);
+    sendData();
+  };
 
-  return <ProductList products={products} />;
+  return (
+    <div>
+      <div>
+        <label htmlFor="limitInput">Limit:</label>
+        <input
+          type="number"
+          id="limitInput"
+          value={limit}
+          onChange={(e) => setLimit(Number(e.target.value))}
+        />
+        <button onClick={handleButtonClick}>Fetch Products</button>
+      </div>
+      <ProductList products={products} />
+    </div>
+  );
 };
 
 export default Index;
